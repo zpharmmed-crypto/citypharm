7e448fdce845f33c83a5be0dec9592c9cf0fe5e3
Abubakr <z.pharmmed@gmail.com>
2025-10-20 15:38:22 +0500

    Fix database and deploy config for Render

--- citypharm/settings.py
+++ citypharm/settings.py
@@ -54,7 +54,7 @@ WSGI_APPLICATION = 'citypharm.wsgi.application'
 # DATABASE
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / "db.sqlite3"),
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
